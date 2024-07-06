51988910328329a703a67cf937677b07a98e5343
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-07-06 19:36:48 +0600

    login-signup & protected homepage

--- Authentication/authentication/base/urls.py
+++ Authentication/authentication/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import authView
+from .views import authView, home
 
 urlpatterns = [
+    path("", home, name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("signup/", authView, name="authView"),
 ]
